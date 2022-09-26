cf89b06b07eb38252eb5647064ea97558f06a141
Sardor Madaminov <sardor0968@gmail.com>
2022-09-26 19:27:25 +0900

    web security{MVC}:added

---
+++ src/main/java/com/example/websecurity/WebSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.websecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/websecurity/WebSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
